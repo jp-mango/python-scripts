b81da4046ec16f296bebbe16f3ecfaf8c2c42608
Justin <menziesjustin@ymail.com>
2023-08-28 21:27:16 -0400

    fixed bugs

--- table-extract.py
+++ table-extract.py
@@ -1,4 +1,19 @@
-#extracts table from wikipedia
 import pandas as pd
-gdp = pd.read_csv('https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)')[2]
-print(gdp)
+
+# URL of the Wikipedia page
+url = 'https://en.wikipedia.org/wiki/FIFA_World_Cup'
+
+# Read HTML tables from the URL
+tables = pd.read_html(url)
+
+# change value for how many tables down
+table_df = tables[3]
+
+# Print the DataFrame
+print(table_df)
+
+# Export the DataFrame to a CSV file
+filename = 'table_data.csv'
+table_df.to_csv(filename, index=False)
+
+print(f"Table saved as: {filename}")
