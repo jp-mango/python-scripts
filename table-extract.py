c9d843a3be0c8ec35954161eb847abb4621298f4
Justin <menziesjustin@ymail.com>
2023-08-29 12:31:48 -0400

    added spacing and comments

--- table-extract.py
+++ table-extract.py
@@ -2,18 +2,13 @@ import pandas as pd
 
 # URL of the Wikipedia page
 url = 'https://en.wikipedia.org/wiki/FIFA_World_Cup'
-
 # Read HTML tables from the URL
 tables = pd.read_html(url)
-
 # change value for how many tables down
 table_df = tables[3]
-
 # Print the DataFrame
 print(table_df)
-
 # Export the DataFrame to a CSV file
 filename = 'table_data.csv'
 table_df.to_csv(filename, index=False)
-
 print(f"Table saved as: {filename}")
