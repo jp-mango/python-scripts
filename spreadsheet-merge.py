cc5e6cde797b8472fd837a57bedd39baa69bd5e7
Justin <menziesjustin@ymail.com>
2023-08-29 18:27:50 -0400

    added python script that merges 2 similar spreadsheets, eliminating duplicates.

---
+++ spreadsheet-merge.py
@@ -0,0 +1,25 @@
+import csv
+# Read data from the first CSV file
+file1_data = set()
+with open('file1.csv', 'r') as file1:
+    csv_reader = csv.reader(file1)
+    next(csv_reader)  # Skip header
+    for row in csv_reader:
+        file1_data.add(tuple(row))
+
+# Read data from the second CSV file
+file2_data = set()
+with open('file2.csv', 'r') as file2:
+    csv_reader = csv.reader(file2)
+    next(csv_reader)  # Skip header
+    for row in csv_reader:
+        file2_data.add(tuple(row))
+
+# Merge the data and remove duplicates
+merged_data = file1_data.union(file2_data)
+
+# Write the merged and deduplicated data to a new CSV file
+with open('merged_file.csv', 'w', newline='') as merged_file:
+    csv_writer = csv.writer(merged_file)
+    csv_writer.writerow(['First Name', 'Last Name'])
+    csv_writer.writerows(merged_data)
