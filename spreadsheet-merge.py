af9a71684571f349f2d292732e9ced4de258f2d4
Justin <menziesjustin@ymail.com>
2023-08-29 18:49:34 -0400

    made changes to the merge section. Now automatically transfers the header as well.

--- spreadsheet-merge.py
+++ spreadsheet-merge.py
@@ -18,8 +18,13 @@ with open('file2.csv', 'r') as file2: #change name to match filename
 # Merge the data and remove duplicates
 merged_data = file1_data.union(file2_data)
 
-# Write the merged and deduplicated data to a new CSV file
+# Read the header from one of the original files
+with open('file1.csv', 'r') as file1:
+    csv_reader = csv.reader(file1)
+    header = next(csv_reader)  # Read the header
+
+# Write the merged and deduplicated data to a new CSV file with the same header
 with open('merged_file.csv', 'w', newline='') as merged_file:
     csv_writer = csv.writer(merged_file)
-    csv_writer.writerow(['First Name', 'Last Name'])
+    csv_writer.writerow(header)  # Write the header
     csv_writer.writerows(merged_data)
