45bd3642397aecd5eb223cb7fbfd590cd9ad3b1c
Justin <menziesjustin@ymail.com>
2023-09-15 11:34:58 -0400

    - added the lambda function i use to count site visits

---
+++ resumeViewCounter-lambda.py
@@ -0,0 +1,30 @@
+import json, boto3
+
+client = boto3.client('dynamodb')
+TableName = 'ResumeSiteVisitors'
+
+def viewCounter(event, context):
+    data = client.get_item(
+        TableName = 'ResumeSiteVisitors',
+        Key = {
+            'stat': {'S':'view-count'}
+        }
+    )
+    prevViewCount = data['Item']['quantity']['N']
+    
+    response = client.update_item(
+        TableName = 'ResumeSiteVisitors',
+        Key = {
+            'stat': {'S':'view-count'}
+        },
+        UpdateExpression = 'ADD quantity :inc',
+        ExpressionAttributeValues = {":inc" : {"N":"1"}},
+        ReturnValues = 'UPDATED_NEW'
+    )
+    
+    value = response['Attributes']['quantity']['N']
+    
+    return{
+        'statusCode': 200,
+        'body': value
+    }
