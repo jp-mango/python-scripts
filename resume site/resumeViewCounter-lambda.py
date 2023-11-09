5857538593a66d359f3cf8f7665fe9a8c6b1c739
Justin <menziesjustin@ymail.com>
2023-11-09 01:28:01 -0500

    - fixed some formatting errors: multiple imports on 1 line, etc.

--- resume site/resumeViewCounter-lambda.py
+++ resume site/resumeViewCounter-lambda.py
@@ -1,39 +1,41 @@
-import json, boto3
+import json
+import boto3
 from botocore.exceptions import ClientError
 
-client = boto3.client('dynamodb')
-TableName = 'ResumeSiteVisitors'
+client = boto3.client("dynamodb")
+TableName = "ResumeSiteVisitors"
+
 
 def viewCounter(event, context):
-    increment = event.get('queryStringParameters', {}).get('increment', 'true').lower() == 'true'
+    increment = (
+        event.get("queryStringParameters", {}).get("increment", "true").lower()
+        == "true"
+    )
 
     try:
-        data = client.get_item(
-            TableName=TableName,
-            Key={'stat': {'S':'view-count'}}
-        )
-        currentCount = data.get('Item', {}).get('quantity', {}).get('N', '0')
-        
+        data = client.get_item(TableName=TableName, Key={"stat": {"S": "view-count"}})
+        currentCount = data.get("Item", {}).get("quantity", {}).get("N", "0")
+
         if increment:
             response = client.update_item(
                 TableName=TableName,
-                Key={'stat': {'S':'view-count'}},
-                UpdateExpression='ADD quantity :inc',
-                ExpressionAttributeValues={":inc" : {"N":"1"}},
-                ReturnValues='UPDATED_NEW'
+                Key={"stat": {"S": "view-count"}},
+                UpdateExpression="ADD quantity :inc",
+                ExpressionAttributeValues={":inc": {"N": "1"}},
+                ReturnValues="UPDATED_NEW",
             )
-            currentCount = response['Attributes']['quantity']['N']
-        
+            currentCount = response["Attributes"]["quantity"]["N"]
+
     except ClientError as e:
-        print(e.response['Error']['Message'])
+        print(e.response["Error"]["Message"])
         return {
-            'statusCode': 500,
-            'body': json.dumps({'error': 'Could not retrieve or update view count'}),
-            'headers': {'Content-Type': 'application/json'}
+            "statusCode": 500,
+            "body": json.dumps({"error": "Could not retrieve or update view count"}),
+            "headers": {"Content-Type": "application/json"},
         }
-    
+
     return {
-        'statusCode': 200,
-        'body': json.dumps({'viewCount': currentCount}),
-        'headers': {'Content-Type': 'application/json'}
+        "statusCode": 200,
+        "body": json.dumps({"viewCount": currentCount}),
+        "headers": {"Content-Type": "application/json"},
     }
