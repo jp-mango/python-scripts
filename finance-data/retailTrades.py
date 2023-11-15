83a3712b09b375468dcca20374fdcd9d10e6ec58
Justin <menziesjustin@ymail.com>
2023-11-15 10:52:27 -0500

    - reformatted

--- finance-data/retailTrades.py
+++ finance-data/retailTrades.py
@@ -6,13 +6,18 @@ import configparser
 
 # Loading configuration from the config.ini file using its absolute path
 config = configparser.ConfigParser()
-config.read(os.path.abspath('C:/Users/menzi/Code/NQ_Tables/retail-activity/retailTrades.ini'))
-api_key = config.get('API', 'api_key')
+config.read(
+    os.path.abspath("C:/Users/menzi/Code/NQ_Tables/retail-activity/retailTrades.ini")
+)
+api_key = config.get("API", "api_key")
 
 # Format: https://data.nasdaq.com/api/v3/datatables/{Table Code}/.{csv,json,xml}?api_key={api}
-table_url = f'https://data.nasdaq.com/api/v3/datatables/NDAQ/RTAT10.csv?&api_key={api_key}'
-output_folder = 'C:/Users/menzi/Code/NQ_Tables/retail-activity'
+table_url = (
+    f"https://data.nasdaq.com/api/v3/datatables/NDAQ/RTAT10.csv?&api_key={api_key}"
+)
+output_folder = "C:/Users/menzi/Code/NQ_Tables/retail-activity"
 os.makedirs(output_folder, exist_ok=True)
+
 
 # Define a function to download and save a CSV file
 def download_and_save_csv(url, table_name):
@@ -26,18 +31,22 @@ def download_and_save_csv(url, table_name):
             file_path = os.path.join(output_folder, file_name)
 
             # Save the CSV content to the file
-            with open(file_path, 'wb') as file:
+            with open(file_path, "wb") as file:
                 file.write(response.content)
             print(f"Downloaded and saved {file_name} to {file_path}")
 
         else:
-            print(f"Request failed with status code {response.status_code}: {response.text}")
+            print(
+                f"Request failed with status code {response.status_code}: {response.text}"
+            )
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
 
+
 # table name
 download_and_save_csv(table_url, "RTAT10")
+
 
 def send_to_discord_with_files(webhook_url, content, file_paths):
     files = {}
@@ -48,11 +57,13 @@ def send_to_discord_with_files(webhook_url, content, file_paths):
         # Extract first 11 rows and save to a new file
         df = pd.read_csv(file_path)
         df_head = df.head(10)
-        temp_file_path = os.path.join(output_folder, f"temp_{os.path.basename(file_path)}")
+        temp_file_path = os.path.join(
+            output_folder, f"temp_{os.path.basename(file_path)}"
+        )
         df_head.to_csv(temp_file_path, index=False)
         temp_files.append(temp_file_path)  # Store path for cleanup
 
-        file = open(temp_file_path, 'rb')
+        file = open(temp_file_path, "rb")
         open_files.append(file)  # Store the file handle
         file_key = f"file{index}"  # 'file0', 'file1', etc.
         files[file_key] = (os.path.basename(temp_file_path), file)
@@ -66,8 +77,12 @@ def send_to_discord_with_files(webhook_url, content, file_paths):
     for temp_file in temp_files:
         os.remove(temp_file)
 
-    if response.status_code != 204:  # 204 is the expected successful response code from Discord for a webhook post
-        print(f"Failed to send files to Discord. Response Code: {response.status_code}. Response: {response.text}")
+    if (
+        response.status_code != 204
+    ):  # 204 is the expected successful response code from Discord for a webhook post
+        print(
+            f"Failed to send files to Discord. Response Code: {response.status_code}. Response: {response.text}"
+        )
 
 
 # Discord webhook info:
@@ -75,12 +90,16 @@ yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
 while yesterday.weekday() > 4:  # 0 = Monday, 1 = Tuesday, ... , 6 = Sunday
     yesterday -= datetime.timedelta(days=1)
 
-yesterday_date = yesterday.strftime('%m/%d/%Y')
-webhook_url = config.get('DISCORD', 'webhook_url')
-message = f"Here's updated __**retail trader activity & sentiment**__ for {yesterday_date}!"
+yesterday_date = yesterday.strftime("%m/%d/%Y")
+webhook_url = config.get("DISCORD", "webhook_url")
+message = (
+    f"Here's updated __**retail trader activity & sentiment**__ for {yesterday_date}!"
+)
 
 # Get all .csv files in the directory
 all_files = os.listdir(output_folder)
-csv_files = [os.path.join(output_folder, file) for file in all_files if file.endswith('.csv')]
+csv_files = [
+    os.path.join(output_folder, file) for file in all_files if file.endswith(".csv")
+]
 
 send_to_discord_with_files(webhook_url, message, csv_files)
--- finance-data/zillowData.py
+++ finance-data/zillowData.py
@@ -1,28 +1,34 @@
 import requests
 import os
 import psycopg2
-import configparser  # Added to read the configuration file
-import datetime
+import configparser
 
 # Loading configuration from the config.ini file using its absolute path
 config = configparser.ConfigParser()
-config.read(os.path.abspath('./zillow_db/zillowData.ini')) # use full filepath if automating
+config.read(
+    os.path.abspath("./zillow_db/zillowData.ini")
+)  # use full filepath if automating
 
 # Define PostgreSQL database connection parameters from the configuration file, and api_key
 db_params = {
-    "host": config.get('DATABASE', 'host'),
-    "database": config.get('DATABASE', 'database_name'),
-    "user": config.get('DATABASE', 'user'),
-    "password": config.get('DATABASE', 'password')
+    "host": config.get("DATABASE", "host"),
+    "database": config.get("DATABASE", "database_name"),
+    "user": config.get("DATABASE", "user"),
+    "password": config.get("DATABASE", "password"),
 }
-api_key = config.get('API', 'api_key')  # API key from configuration file
+api_key = config.get("API", "api_key")  # API key from configuration file
 
 # Define the API endpoints for CSV files, and local save
-zillow_data_url = f'https://data.nasdaq.com/api/v3/datatables/ZILLOW/DATA.csv?&api_key={api_key}'
-zillow_indicators_url = f'https://data.nasdaq.com/api/v3/datatables/ZILLOW/INDICATORS.csv?&api_key={api_key}'
-zillow_regions_url = f'https://data.nasdaq.com/api/v3/datatables/ZILLOW/REGIONS.csv?&api_key={api_key}'
-output_folder = "zillow_db" # use full filepath if automating
+zillow_data_url = (
+    f"https://data.nasdaq.com/api/v3/datatables/ZILLOW/DATA.csv?&api_key={api_key}"
+)
+zillow_indicators_url = f"https://data.nasdaq.com/api/v3/datatables/ZILLOW/INDICATORS.csv?&api_key={api_key}"
+zillow_regions_url = (
+    f"https://data.nasdaq.com/api/v3/datatables/ZILLOW/REGIONS.csv?&api_key={api_key}"
+)
+output_folder = "zillow_db"  # use full filepath if automating
 os.makedirs(output_folder, exist_ok=True)
+
 
 # Define a function to download and save a CSV file
 def download_and_save_csv(url, table_name):
@@ -33,10 +39,10 @@ def download_and_save_csv(url, table_name):
         if response.status_code == 200:
             # Construct the file path to save the CSV
             file_name = f"{table_name}.csv"
-            file_path = os.path.join(output_folder, file_name) 
+            file_path = os.path.join(output_folder, file_name)
 
             # Save the CSV content to the file
-            with open(file_path, 'wb') as file:
+            with open(file_path, "wb") as file:
                 file.write(response.content)
             print(f"Downloaded and saved {file_name} to {file_path}")
 
@@ -44,10 +50,13 @@ def download_and_save_csv(url, table_name):
             upload_csv_to_postgres(file_path, table_name)
 
         else:
-            print(f"Request failed with status code {response.status_code}: {response.text}")
+            print(
+                f"Request failed with status code {response.status_code}: {response.text}"
+            )
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
+
 
 # Use a context manager for database connection handling
 class DatabaseConnection:
@@ -59,24 +68,28 @@ class DatabaseConnection:
         if self.conn:
             self.conn.close()
 
-# upload csv to postgress
+
+# upload csv to postgres
 def upload_csv_to_postgres(csv_path, table_name):
     try:
-        with open(csv_path, 'r') as f:
+        with open(csv_path, "r") as f:
             cursor = conn.cursor()
-            
+
             # Clear the table before inserting new data
-            cursor.execute(f"TRUNCATE TABLE public.{table_name} RESTART IDENTITY CASCADE")
-            
+            cursor.execute(
+                f"TRUNCATE TABLE public.{table_name} RESTART IDENTITY CASCADE"
+            )
+
             # Use the COPY command to load data from the CSV file into the table
             cursor.copy_expert(f"COPY public.{table_name} FROM stdin CSV HEADER", f)
-            
+
         conn.commit()
         print(f"Uploaded data from {csv_path} to public.{table_name} \n")
 
     except Exception as e:
         print(f"Error uploading data to public.{table_name}: {str(e)}")
         conn.rollback()  # Rollback the current transaction to prevent it from affecting subsequent uploads
+
 
 # Wrap the downloading and uploading process in a context manager for the database connection
 with DatabaseConnection() as conn:
